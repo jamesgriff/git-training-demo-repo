1c495a9fd6cfdcaba930327fdbac12a0216df7df
Alice <alice@example.com>
2022-08-23 17:58:49 +0100

    Section headings: Add section headings

--- My first R script.R
+++ My first R script.R
@@ -1,12 +1,16 @@
 
 print("This is my first R script")
 
+print("Input")
 print("TODO: Open the CSV file")
 
+print("Pre-processing")
 print("TODO: Re-name some columns")
 
 print("TODO: Correct the spelling of some cell values")
 
+print("Processing")
 print("TODO: Calculate some averages")
 
+print("Output")
 print("TODO: Save a new CSV with the averages")
