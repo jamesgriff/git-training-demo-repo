f219aa1ffaebb87855d8d82b5f75ebed557d9ce4
James Griffiths <james@jwgsoftware.com>
2022-08-23 17:47:11 +0100

    Add a step: Correct the spelling of some cell values

--- My first R script.R
+++ My first R script.R
@@ -5,6 +5,8 @@ print("TODO: Open the CSV file")
 
 print("TODO: Re-name some columns")
 
+print("TODO: Correct the spelling of some cell values")
+
 print("TODO: Calculate some averages")
 
 print("TODO: Save a new CSV with the averages")
