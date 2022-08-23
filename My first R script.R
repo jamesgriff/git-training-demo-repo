8f7f2a22a827ca45d79493486e1bb067ce022565
James Griffiths <james@jwgsoftware.com>
2022-08-23 17:46:49 +0100

    Add some pseudo-code

--- My first R script.R
+++ My first R script.R
@@ -1,2 +1,10 @@
 
 print("This is my first R script")
+
+print("TODO: Open the CSV file")
+
+print("TODO: Re-name some columns")
+
+print("TODO: Calculate some averages")
+
+print("TODO: Save a new CSV with the averages")
