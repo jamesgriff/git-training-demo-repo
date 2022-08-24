32f766eac03edde77a2eb00cc442f748432f9db7
David <david@example.com>
2022-08-24 12:00:00 +0100

    Add extra content to the report

--- My first R script.R
+++ My first R script.R
@@ -16,5 +16,6 @@ print("OUTPUT")
 print("TODO: Save a new CSV with the averages")
 
 print("TODO: Create a report of the tasks we just performed")
+print("TODO: Add extra content to the report")
 
 print("TODO: Email the report to bob@example.com")
