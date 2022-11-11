8a0f57bcdaf22ea876c90f5b03ea381c38e2f357
James Griffiths <james@jwgsoftware.com>
2022-11-11 14:56:25 +0000

    Fix email address

--- My first R script.R
+++ My first R script.R
@@ -17,4 +17,4 @@ print("TODO: Save a new CSV with the averages")
 
 print("TODO: Create a report of the tasks we just performed")
 
-print("TODO: Email the report to analysis-team@example.com")
+print("TODO: Email the report to team-analysis@example.com")
