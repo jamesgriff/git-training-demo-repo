66cb08f8fd25d341fbb080b5c76a9eb30894ced8
James Griffiths <james@jwgsoftware.com>
2022-11-22 13:44:35 +0000

    Add folders and docs

---
+++ 3-produce-outputs/report-summary.R
@@ -0,0 +1,26 @@
+
+print("This is my first R script")
+
+print("** INPUT **")
+print("TODO: Open the CSV file")
+
+print("** PRE-PROCESSING **")
+print("TODO: Re-name some columns")
+
+print("TODO: Correct the spelling of some cell values")
+
+print("** PROCESSING **")
+print("TODO: Calculate some averages")
+
+print("** OUTPUT **")
+print("TODO: Save a new CSV with the averages")
+
+print("TODO: Create a report of the tasks we just performed")
+
+print("TODO: Email the report to team-analysis@example.com")
+
+print("Another change as a demo")
+
+print("More main branch changes ON LAPTOP")
+
+print("Main branch changes on GITHUB")
